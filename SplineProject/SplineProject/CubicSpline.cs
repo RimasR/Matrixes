c0e5ea01ec161fc78560a6f24610e64208d71bc0
Rimantas Radžiūnas <rimantas.radziunas@mif.stud.vu.lt>
2016-11-03 09:47:05 +0200

    -

---
+++ SplineProject/SplineProject/CubicSpline.cs
@@ -0,0 +1,174 @@
+﻿using System;
+
+namespace SplineProject
+{
+    public class CubicSpline
+    {
+        // N-1 spline coefficients for N points
+        private float[] a;
+
+        private float[] b;
+
+        // Save the original x and y for Eval
+        private float[] xOrig;
+
+        private float[] yOrig;
+
+        private int _lastIndex = 0;
+
+        public CubicSpline()
+        {
+            
+        }
+
+        public CubicSpline(float[] x, float[] y, float startSlope = float.NaN, float endSlope = float.NaN, bool debug = false)
+        {
+            Fit(x, y, startSlope, endSlope, debug);
+        }
+
+        public static float[] Compute(float[] x, float[] y, float[] xs, float startSlope = float.NaN, float endSlope = float.NaN, bool debug = false)
+        {
+            CubicSpline spline = new CubicSpline();
+            return spline.FitAndEval(x, y, xs, startSlope, endSlope, debug);
+        }
+
+        public float[] FitAndEval(float[] x, float[] y, float[] xs, float startSlope = float.NaN, float endSlope = float.NaN, bool debug = false)
+        {
+            Fit(x, y, startSlope, endSlope, debug);
+            return Eval(xs, debug);
+        }
+
+        private void Fit(float[] x, float[] y, float startSlope = float.NaN, float endSlope = float.NaN, bool debug = false)
+        {
+            if (Single.IsInfinity(startSlope) || Single.IsInfinity(endSlope))
+            {
+                throw new Exception("Start or end cannot be infinity!");
+            }
+
+            // save original x and y
+            this.xOrig = x;
+            this.yOrig = y;
+
+            int n = x.Length;
+            float[] r = new float[n]; // right side numbers.
+
+            TriDiagonalMatrix m = new TriDiagonalMatrix(n);
+            float dx1, dx2, dy1, dy2;
+
+            if (float.IsNaN(startSlope))
+            {
+                dx1 = x[1] - x[0];
+                m.C[0] = 1.0f / dx1;
+                m.B[0] = 2.0f * m.C[0];
+                r[0] = 3 * (y[1] - y[0]) / (dx1 * dx1);
+            }
+            else
+            {
+                m.B[0] = 1;
+                r[0] = startSlope;
+            }
+
+            // Body rows (equation 15 from the article)
+            for (int i = 1; i < n - 1; i++)
+            {
+                dx1 = x[i] - x[i - 1];
+                dx2 = x[i + 1] - x[i];
+
+                m.A[i] = 1.0f / dx1;
+                m.C[i] = 1.0f / dx2;
+                m.B[i] = 2.0f * (m.A[i] + m.C[i]);
+
+                dy1 = y[i] - y[i - 1];
+                dy2 = y[i + 1] - y[i];
+                r[i] = 3 * (dy1 / (dx1 * dx1) + dy2 / (dx2 * dx2));
+            }
+
+            // Last row also different (equation 17 from the article)
+            if (float.IsNaN(endSlope))
+            {
+                dx1 = x[n - 1] - x[n - 2];
+                dy1 = y[n - 1] - y[n - 2];
+                m.A[n - 1] = 1.0f / dx1;
+                m.B[n - 1] = 2.0f * m.A[n - 1];
+                r[n - 1] = 3 * (dy1 / (dx1 * dx1));
+            }
+            else
+            {
+                m.B[n - 1] = 1;
+                r[n - 1] = endSlope;
+            }
+
+            if (debug) Console.WriteLine("Tri-diagonal matrix:\n{0}", m.ToDisplayString(":0.0000", "  "));
+            if (debug) Console.WriteLine("r: {0}", ArrayUtil.ToString<float>(r));
+
+            // k is the solution to the matrix
+            float[] k = m.Solve(r);
+            if (debug) Console.WriteLine("k = {0}", ArrayUtil.ToString<float>(k));
+
+            // a and b are each spline's coefficients
+            this.a = new float[n - 1];
+            this.b = new float[n - 1];
+
+            for (int i = 1; i < n; i++)
+            {
+                dx1 = x[i] - x[i - 1];
+                dy1 = y[i] - y[i - 1];
+                a[i - 1] = k[i - 1] * dx1 - dy1; // equation 10 from the article
+                b[i - 1] = -k[i] * dx1 + dy1; // equation 11 from the article
+            }
+
+            if (debug) Console.WriteLine("a: {0}", ArrayUtil.ToString<float>(a));
+            if (debug) Console.WriteLine("b: {0}", ArrayUtil.ToString<float>(b));
+        }
+
+        public float[] Eval(float[] x, bool debug = false)
+        {
+            CheckAlreadyFitted();
+
+            int n = x.Length;
+            float[] y = new float[n];
+            _lastIndex = 0; // Reset simultaneous traversal in case there are multiple calls
+
+            for (int i = 0; i < n; i++)
+            {
+                // Find which spline can be used to compute this x (by simultaneous traverse)
+                int j = GetNextXIndex(x[i]);
+
+                // Evaluate using j'th spline
+                y[i] = EvalSpline(x[i], j, debug);
+            }
+
+            return y;
+        }
+
+        private void CheckAlreadyFitted()
+        {
+            if (a == null) throw new Exception("Fit must be called before you can evaluate.");
+        }
+
+        private int GetNextXIndex(float x)
+        {
+            if (x < xOrig[_lastIndex])
+            {
+                throw new ArgumentException("The X values to evaluate must be sorted.");
+            }
+
+            while ((_lastIndex < xOrig.Length - 2) && (x > xOrig[_lastIndex + 1]))
+            {
+                _lastIndex++;
+            }
+
+            return _lastIndex;
+        }
+
+        private float EvalSpline(float x, int j, bool debug = false)
+        {
+            float dx = xOrig[j + 1] - xOrig[j];
+            float t = (x - xOrig[j]) / dx;
+            float y = (1 - t) * yOrig[j] + t * yOrig[j + 1] + t * (1 - t) * (a[j] * (1 - t) + b[j] * t); // equation 9
+            if (debug) Console.WriteLine("xs = {0}, j = {1}, t = {2}", x, j, t);
+            return y;
+        }
+
+    }
+}
--- SplineProject/SplineProject/Program.cs
+++ SplineProject/SplineProject/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml.Serialization;
 
 namespace SplineProject
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             SolveMatrix();
+            SolveSpline();
+            SolveSplineWithFunction();
         }
 
         private static TriDiagonalMatrix SolveMatrix()
@@ -44,6 +48,141 @@ namespace SplineProject
 
             Console.WriteLine();
             return matrix;
+        }
+
+        private static void SolveSpline()
+        {
+            float[] x = { 0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
+            float[] y = { 0.0f, 0.0f, 209.0f, 209.0f, 0.0f, 209.0f };
+
+            int n = x.Length;
+
+            int upsampleFactor = 10;
+            int nInterpolated = n*upsampleFactor;
+            float[] xs = new float[nInterpolated];
+
+            for (int i = 0; i < nInterpolated; i++)
+            {
+                xs[i] = (float) i*(n - 1)/(float) (nInterpolated - 1);
+            }
+
+            float[] ys = CubicSpline.Compute(x, y, xs, 0.0f, 6.0f, true);
+
+            string path = @"..\..\manosplainasbefunkcijos.png";
+            PlotSplineSolution("Cubic Spline Interpolation", x, y, xs, ys, path);
+        }
+
+        private static float SolveFunction(float x)
+        {
+
+            double answer = Math.Sin(2*x) - 0.2*(x*x);
+
+            return (float) answer;
+        }
+
+        private static void SolveSplineWithFunction()
+        {
+            float[] x = { 0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
+            int n = x.Length;
+            float[] y = new float[n];
+            for (int i = 0; i < n; i++)
+            {
+                y[i] = SolveFunction(x[i]);
+            }
+
+            int upsampleFactor = 10;
+            int nInterpolated = n * upsampleFactor;
+            float[] xs = new float[nInterpolated];
+
+            for (int i = 0; i < nInterpolated; i++)
+            {
+                xs[i] = (float)i * (n - 1) / (float)(nInterpolated - 1);
+            }
+
+            float[] ys = CubicSpline.Compute(x, y, xs, 0.0f, 6.0f, true);
+
+            string path = @"..\..\manosplainassufunkcija.png";
+            PlotSplineSolution("Cubic Spline Interpolation", x, y, xs, ys, path);
+        }
+
+        private static void PlotSplineSolution(string title, float[] x, float[] y, float[] xs, float[] ys, string path, float[] qPrime = null)
+        {
+            var chart = new Chart();
+            chart.Size = new Size(600, 400);
+            chart.Titles.Add(title);
+            chart.Legends.Add(new Legend("Legend"));
+
+            ChartArea ca = new ChartArea("DefaultChartArea");
+            ca.AxisX.Title = "X";
+            ca.AxisY.Title = "Y";
+            chart.ChartAreas.Add(ca);
+
+            Series s1 = CreateSeries(chart, "Spline", CreateDataPoints(xs, ys), Color.Blue, MarkerStyle.None);
+            Series s2 = CreateSeries(chart, "Original", CreateDataPoints(x, y), Color.Green, MarkerStyle.Diamond);
+
+            chart.Series.Add(s2);
+            chart.Series.Add(s1);
+
+            if (qPrime != null)
+            {
+                Series s3 = CreateSeries(chart, "Slope", CreateDataPoints(xs, qPrime), Color.Red, MarkerStyle.None);
+                chart.Series.Add(s3);
+            }
+
+            ca.RecalculateAxesScale();
+            ca.AxisX.Minimum = Math.Floor(ca.AxisX.Minimum);
+            ca.AxisX.Maximum = Math.Ceiling(ca.AxisX.Maximum);
+            int nIntervals = (x.Length - 1);
+            nIntervals = Math.Max(4, nIntervals);
+            ca.AxisX.Interval = (ca.AxisX.Maximum - ca.AxisX.Minimum) / nIntervals;
+
+            // Save
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            using (FileStream fs = new FileStream(path, FileMode.CreateNew))
+            {
+                chart.SaveImage(fs, ChartImageFormat.Png);
+            }
+        }
+
+        private static List<DataPoint> CreateDataPoints(float[] x, float[] y)
+        {
+            Debug.Assert(x.Length == y.Length);
+            List<DataPoint> points = new List<DataPoint>();
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                points.Add(new DataPoint(x[i], y[i]));
+            }
+
+            return points;
+        }
+
+        private static Series CreateSeries(Chart chart, string seriesName, IEnumerable<DataPoint> points, Color color, MarkerStyle markerStyle = MarkerStyle.None)
+        {
+            var s = new Series()
+            {
+                XValueType = ChartValueType.Double,
+                YValueType = ChartValueType.Double,
+                Legend = chart.Legends[0].Name,
+                IsVisibleInLegend = true,
+                ChartType = SeriesChartType.Line,
+                Name = seriesName,
+                ChartArea = chart.ChartAreas[0].Name,
+                MarkerStyle = markerStyle,
+                Color = color,
+                MarkerSize = 8
+            };
+
+            foreach (var p in points)
+            {
+                s.Points.Add(p);
+            }
+
+            return s;
         }
     }
 }
